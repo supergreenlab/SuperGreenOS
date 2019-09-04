bfe6f1e60d0d7d443f921bb57161743db21cf920
vitaminwater <stantin04@gmail.com>
2019-09-04 15:45:05 +0200

    updated update_html script, fixed network spamming in admin interface

--- html_app/utils.js
+++ html_app/utils.js
@@ -1,9 +1,45 @@
 const URL = 'http://192.168.1.11'
+const DEBUG = false
+
+const schedule_promise = (n, retries) => {
+  let loading_param_promise = Promise.resolve(),
+      promises = []
+  return function(req_func, ret_func) {
+    let resolve, reject
+    const p = new Promise((res, rej) => {resolve = res; reject = rej})
+    loading_param_promise.then(async () => {
+      let error
+      for (let i = 0; i < retries; ++i) {
+        try {
+          resolve(await req_func())
+          return
+        } catch(e) {
+          error = e
+          if (e.status == 404) {
+            break
+          }
+          ret_func && ret_func(e, i + 1)
+        }
+      }
+      reject(error)
+    })
+    promises.push(p)
+    if (promises.length >= n) {
+      loading_param_promise = Promise.all(promises).catch((e) => {
+        console.log('promise.all', e)
+      })
+      promises = []
+    }
+
+    return p
+  }
+}
+const fetchQueue = schedule_promise(3, 3)
 
 const fetchConfig = async function() {
   return new Promise(function(resolve, reject) {
     const r = new XMLHttpRequest()
-    r.open('GET', `config.json?id=${parseInt(Math.random()*999999)}`, true)
+    r.open('GET', DEBUG ? '/config.json' : '/fs/config.json', true)
     r.onreadystatechange = function () {
       if (r.readyState != 4) return
       if (r.status != 200) {
@@ -17,9 +53,9 @@ const fetchConfig = async function() {
 }
 
 const fetchParam = async function(type, paramName) {
-  return new Promise(function(resolve, reject) {
+  return fetchQueue(() => new Promise(function(resolve, reject) {
     const r = new XMLHttpRequest()
-    r.open('GET', `${URL}/${type}?k=${paramName}`, true)
+    r.open('GET', `${DEBUG ? URL : ''}/${type}?k=${paramName}`, true)
     r.onreadystatechange = function () {
       if (r.readyState != 4) return
       if (r.status != 200) {
@@ -33,13 +69,13 @@ const fetchParam = async function(type, paramName) {
       }
     }
     r.send()
-  })
+  }))
 }
 
 const updateParam = async function(type, paramName, value) {
-  return new Promise(function(resolve, reject) {
+  return fetchQueue(() => new Promise(function(resolve, reject) {
     const r = new XMLHttpRequest()
-    r.open('POST', `${URL}/${type}?k=${paramName}&v=${value}`, true)
+    r.open('POST', `${DEBUG ? URL : ''}/${type}?k=${paramName}&v=${value}`, true)
     r.onreadystatechange = function () {
       if (r.readyState != 4) return
       if (r.status != 200) {
@@ -49,9 +85,5 @@ const updateParam = async function(type, paramName, value) {
       resolve(r.responseText);
     }
     r.send()
-  })
-}
-
-const updateDiv = function(id, value) {
-  document.getElementById(id).innerText = value
+  }))
 }
