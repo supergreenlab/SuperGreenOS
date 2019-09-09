ad63f72d67784404253df664ee8f8fb12c43adcd
vitaminwater <stantin04@gmail.com>
2019-09-09 14:38:02 +0200

    updated template files from SuperGreenOSBoilerplate

--- update_templates.sh
+++ update_templates.sh
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set -e
-
 DATA="$1"
 
 if [ ! -f "$DATA" ]; then
