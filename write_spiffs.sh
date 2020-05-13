e07d8d6fd627f38d8ccd10faaa1649108ba29688
DarthHTTP <int0therain@protonmail.com>
2020-05-13 11:46:08 +0200

    Adds SERIAL_PORT

--- write_spiffs.sh
+++ write_spiffs.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+. ${0%/*}/build.env
+
 GREEN="\033[0;32m"
 NC="\033[0m"
+SERIAL_PORT=${SERIAL_PORT:-"/dev/ttyUSB1"}
 
 echo -e "Creating spiffs.bin partition:"
 rm -rf spiffs_fs_gz
@@ -15,6 +18,6 @@ done
 mkspiffs -c spiffs_fs_gz/ -b 4096 -p 256 -s 0x8000 spiffs.bin
 echo -e "Created spiffs.bin: ${GREEN}Done${NC}"
 
-python $IDF_PATH/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 115200 write_flash -z 0x3f0000 spiffs.bin
+python $IDF_PATH/components/esptool_py/esptool/esptool.py --chip esp32 --port ${SERIAL_PORT} --baud 115200 write_flash -z 0x3f0000 spiffs.bin
 rm spiffs.bin
 rm -rf spiffs_fs_gz
