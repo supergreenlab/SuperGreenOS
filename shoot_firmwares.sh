c59f8eadfe750ddc8d5caf0ff77c94ecb8ac1ae5
vitaminwater <stantin04@gmail.com>
2024-03-17 10:41:26 +0100

    fixed wrong key size

--- shoot_firmwares.sh
+++ shoot_firmwares.sh
@@ -8,7 +8,7 @@ if [ $# -eq "1" ]; then
   SKIP_APP="$1"
 fi
 
-./update_config.sh config_gen/config/SuperGreenOS/Solo config.solo.json
+./update_config.sh config_gen/config/SuperGreenOS/Controllers/Solo config.solo.json
 ./update_templates.sh config.solo.json
 ./update_htmlapp.sh config.solo.json
 ./update_firmware.sh SoloV2.1Testing
@@ -20,7 +20,7 @@ if [ $SKIP_APP -eq "0" ]; then
   cp -r ./releases/SoloV2.1Testing ../backend/SuperGreenCloud/www/update/
 fi
 
-./update_config.sh config_gen/config/SuperGreenOS/Pickle config.pickle.json
+./update_config.sh config_gen/config/SuperGreenOS/Controllers/Pickle config.pickle.json
 ./update_templates.sh config.pickle.json
 ./update_htmlapp.sh config.pickle.json
 ./update_firmware.sh PickleV2.1Testing
@@ -32,7 +32,7 @@ if [ $SKIP_APP -eq "0" ]; then
   cp -r ./releases/PickleV2.1Testing ../backend/SuperGreenCloud/www/update/
 fi
 
-./update_config.sh config_gen/config/SuperGreenOS/Driver config.driver.json
+./update_config.sh config_gen/config/SuperGreenOS/Controllers/Driver config.driver.json
 ./update_templates.sh config.driver.json
 ./update_htmlapp.sh config.driver.json
 ./update_firmware.sh DriverV2.1Testing
@@ -44,7 +44,7 @@ if [ $SKIP_APP -eq "0" ]; then
   cp -r ./releases/DriverV2.1Testing ../backend/SuperGreenCloud/www/update/
 fi
 
-./update_config.sh config_gen/config/SuperGreenOS/Controller/v2.1 config.controller.json
+./update_config.sh config_gen/config/SuperGreenOS/Controllers/Controller/v2.1 config.controller.json
 ./update_templates.sh config.controller.json
 ./update_htmlapp.sh config.controller.json
 ./update_firmware.sh ControllerV2.1Testing
@@ -56,7 +56,7 @@ if [ $SKIP_APP -eq "0" ]; then
   cp -r ./releases/ControllerV2.1Testing ../backend/SuperGreenCloud/www/update/
 fi
 
-./update_config.sh config_gen/config/SuperGreenOS/Controller/v3 config.controller.json
+./update_config.sh config_gen/config/SuperGreenOS/Controllers/Controller/v3 config.controller.json
 ./update_templates.sh config.controller.json
 ./update_htmlapp.sh config.controller.json
 ./update_firmware.sh ControllerV3Testing
